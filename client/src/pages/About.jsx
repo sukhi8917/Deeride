294fadac4a222a1f4fe5a11583d23bb10bf211eb
Sukram <sukramkumar71@gmail.com>
2024-11-17 00:59:53 +0530

    first commit

---
+++ client/src/pages/About.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+export default function About() {
+  return (
+    <div className='py-20 px-4 max-w-6xl mx-auto'>
+      <h1 className='text-3xl font-bold mb-4 text-slate-800'>About WayGO Motors</h1>
+      <p className='mb-4 text-slate-700'>
+      WayGO Motors is a premier car showroom specializing in offering a wide range of new and pre-owned vehicles. We are dedicated to helping customers find their dream cars while providing an exceptional buying experience with top-notch service.
+      </p>
+      <p className='mb-4 text-slate-700'>
+        Our mission is to make your car-buying journey seamless and enjoyable by offering expert guidance, personalized recommendations, and a comprehensive understanding of the automotive market. Whether you’re looking for a sleek sedan, a spacious SUV, or a high-performance sports car, Sahand Motors has something for everyone.
+      </p>
+      <p className='mb-4 text-slate-700'>
+        With years of experience in the automotive industry, our team is passionate about delivering unparalleled customer satisfaction. From assisting with financing options to ensuring smooth vehicle delivery, we are committed to making every step of your car-buying journey stress-free and rewarding.
+      </p>
+    </div>
+  );
+}
